1cac260bd531d718b8ed86deac1924893c39e4c8
Shakib <shakibsmall25@gmail.com>
2023-10-05 12:13:57 -0400

    @astrojs/vercel/serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/serverless";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), sitemap(), partytown()],
-  output: "server",
+  output: 'server',
   adapter: vercel({
-    analytics: true
+    webAnalytics: {
+      enabled: true,
+    },
   }),
 });
